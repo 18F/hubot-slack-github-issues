f69e0b08c3d34f8be299ff9ab54bf4b0d394bac6
Mike Bland <michael.bland@gsa.gov>
2015-12-11 18:03:53 -0500

    Compare expected API params with actual params

--- test/github-client-test.js
+++ test/github-client-test.js
@@ -11,14 +11,19 @@ var expect = chai.expect;
 chai.should();
 chai.use(chaiAsPromised);
 
-function FakeGitHubApi(urlOnSuccess, msgOnError) {
-  var that = this;
-
+function FakeGitHubApi(urlOnSuccess, msgOnError, expectedParams) {
   this.issues = {
     create: function(params, done) {
-      that.params = params;
+      var actualString = JSON.stringify(params),
+          expectedString = JSON.stringify(expectedParams);
 
-      if (urlOnSuccess) {
+      if (actualString !== expectedString) {
+        done(new Error([
+            'params did not match expectations:',
+            'expected: ' + expectedString,
+            'actual:   ' + actualString
+          ].join('\n  ')));
+      } else if (urlOnSuccess) {
         done(null, { url: urlOnSuccess });
       } else {
         done(new Error(msgOnError));
@@ -35,11 +40,18 @@ describe('GitHubClient', function() {
   };
 
   var metadata = {
-    url: 'https://18F.slack.com/archives/hub/p1360782804083113',
-    title: 'Update from @mikebland in #hub at 1360782804.083113'
+    url: 'https://18F.slack.com/archives/handbook/p1360782804083113',
+    title: 'Update from @mikebland in #handbook at 1360782804.083113'
   };
 
   var message = { text: 'Hello, world!' };
+
+  var expectedParams = {
+    user:  config.githubUser,
+    repo:  'handbook',
+    title: metadata.title,
+    body:  'From ' + metadata.url + ':\n\n' + message.text
+  };
 
   it('should create a githubApiClient from the configuration', function() {
     var client = new GitHubClient(config);
@@ -53,7 +65,7 @@ describe('GitHubClient', function() {
 
   it('should successfully file an issue', function() {
     var issueUrl = 'https://github.com/18F/handbook/issues/1',
-        api = new FakeGitHubApi(issueUrl),
+        api = new FakeGitHubApi(issueUrl, null, expectedParams),
         client = new GitHubClient(config, api);
     return client.fileNewIssue(metadata, 'handbook', message)
       .should.eventually.equal(issueUrl);
@@ -61,7 +73,7 @@ describe('GitHubClient', function() {
 
   it('should receive an error when filing an issue', function() {
     var errorMsg = 'failed to file an issue',
-        api = new FakeGitHubApi(null, errorMsg),
+        api = new FakeGitHubApi(null, errorMsg, expectedParams),
         client = new GitHubClient(config, api);
     return client.fileNewIssue(metadata, 'handbook', message)
       .should.be.rejectedWith(Error, errorMsg);
