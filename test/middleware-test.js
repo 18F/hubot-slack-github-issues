f9df20d7beab64dd70521ea80ac765404badad16
Mike Bland <michael.bland@gsa.gov>
2015-12-18 16:35:01 -0500

    Avoid multiple requests for a message, 2 of 3
    Part of #17. This prevents new GitHub requests from being filed when a message
    has already had a GitHub issue filed. The next commit will:
    
    - add `config.successResponse` to messages that have had GitHub issues
      successfully filed

--- test/middleware-test.js
+++ test/middleware-test.js
@@ -9,7 +9,6 @@ var GitHubClient = require('../lib/github-client');
 var SlackClient = require('../lib/slack-client');
 var scriptName = require('../package.json').name;
 var helpers = require('./helpers');
-var config = require('./helpers/test-config.json');
 var FakeSlackClientImpl = require('./helpers/fake-slack-client-impl');
 var LogHelper = require('./helpers/log-helper');
 var sinon = require('sinon');
@@ -23,14 +22,14 @@ chai.use(chaiAsPromised);
 chai.use(chaiThings);
 
 describe('Middleware', function() {
-  var rules, slackClientImpl, githubClient, middleware;
+  var config, slackClientImpl, githubClient, middleware;
 
   beforeEach(function() {
-    rules = helpers.baseConfig().rules;
+    config = helpers.baseConfig();
     slackClientImpl = new FakeSlackClientImpl('handbook');
     githubClient = new GitHubClient(helpers.baseConfig(), {});
     middleware = new Middleware(
-      rules, new SlackClient(slackClientImpl, config), githubClient);
+      config, new SlackClient(slackClientImpl, config), githubClient);
   });
 
   describe('parseMetadata', function() {
@@ -44,7 +43,7 @@ describe('Middleware', function() {
   describe('findMatchingRule', function() {
     it('should find the rule matching the message', function() {
       var message = helpers.reactionAddedMessage().rawMessage,
-          expected = rules[rules.length - 1],
+          expected = config.rules[config.rules.length - 1],
           result = middleware.findMatchingRule(message);
 
       result.reactionName.should.equal(expected.reactionName);
@@ -217,6 +216,27 @@ describe('Middleware', function() {
         logHelper.messages.should.include.something.that.deep.equals(
           inProgressLogMessage);
       }).should.notify(done);
+    });
+
+    it('should not file another issue for the same message when ' +
+      'one is already filed ', function() {
+      var result, alreadyFiledLogMessage;
+
+      message.rawMessage.item.message.reactions.push({
+        name: config.successReaction,
+        count: 1,
+        users: [ helpers.USER_ID ]
+      });
+
+      logHelper.captureLog();
+      result = middleware.execute(context, next, hubotDone);
+      logHelper.restoreLog();
+      expect(result).to.be.undefined;
+
+      alreadyFiledLogMessage = [
+        scriptName + ': ' + helpers.MSG_ID + ': already processed'];
+      logHelper.messages.should.include.something.that.deep.equals(
+        alreadyFiledLogMessage);
     });
   });
 });
