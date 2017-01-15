2111a10bf3cb065799b785b53cd5fb984541cb9e
Greg Walker <mgwalker@gmail.com>
2017-01-15 15:02:50 -0600

    Make it a simple wrapper around Mike Bland's slack-github-issues

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-var path = require('path');
-var Logger = require('./lib/logger');
-
 module.exports = function(robot) {
-  var logger = new Logger(robot.logger);
-
-  logger.info(null, 'loading');
-  robot.loadFile(path.resolve(__dirname, 'scripts'), 'slack-github-issues.js');
+  require('slack-github-issues').loadHubotScript(robot);
 };
